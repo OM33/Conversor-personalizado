10d20e6beb09c061fd7c6b28142a04574f0ac64f
OM33 <olgaems315@gmail.com>
2024-10-17 09:42:03 -0300

    2024 update

--- js/ajax.js
+++ js/ajax.js
@@ -1,204 +1,154 @@
-
-var spinner = $('#sp');
+var spinner = $("#sp");
 
 spinner.hide();
 
-
-var urlBtc = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-var urlDt = "https://s3.amazonaws.com/dolartoday/data.json";
-var urlArs = "http://ws.geeklab.com.ar/dolar/get-dolar-json.php";
-
-function getFetch(url) {
-    return fetch(url);
-}
-
-function getArs(data) {
-    console.log("Chee Somos Argentinos yeeey", data);
-    let ARS = parseFloat(data.libre);
-    ARS = ARS + ((ARS * 30) / 100);
-    return ARS;
-}
-function getBtc(data) {
-    console.log('Viva satoshi nakamotooo woohooo ', data);
-    console.log('Bitcoins');
-    let BTC = 1 / parseFloat(data.bpi.USD.rate_float);
-    return BTC;
-}
-function getBss(data) {
-    console.log('Viva chaveee.... no mentira ', data);
-    console.log('Bolivares Soberanos');
-    return data.USD.transferencia;
-}
-
-
+var urlBtc = "https://api.coindesk.com/v1/bpi/currentprice.json";
+var urlDt = "https://ve.dolarapi.com/v1/dolares/oficial";
+var urlArs = "https://dolarapi.com/v1/dolares/blue";
 
 function moneda(nombre, tipoDeCambio, url) {
-    this.nombre = nombre;
-    this.tipoDeCambio = tipoDeCambio;
-    this.url = url;
+  this.nombre = nombre;
+  this.tipoDeCambio = tipoDeCambio;
+  this.url = url;
 
-    this.obtenerTipoDeMoneda = function (datos) {
-        if (this.nombre == 'ARS') {
-            this.tipoDeCambio = getArs(datos);
-        } else if (this.nombre == 'BTC') {
-            this.tipoDeCambio = getBtc(datos);
-        } else if (this.nombre == 'BSS') {
-            this.tipoDeCambio = getBss(datos);
-        }
-        else if (this.nombre == 'USD'){
-            console.log('dolares americanos');
-            this.tipoDeCambio = 1;
-        }
+  this.obtenerTipoDeMoneda = function (datos) {
+    if (this.nombre == "ARS") {
+      this.tipoDeCambio = datos.venta;
+    } else if (this.nombre == "BTC") {
+      this.tipoDeCambio = 1 / parseFloat(datos.bpi.USD.rate_float);
+    } else if (this.nombre == "BSS") {
+      this.tipoDeCambio = datos.promedio;
+    } else if (this.nombre == "USD") {
+      this.tipoDeCambio = 1;
     }
+  };
 
-
-    this.convertir = function (otramoneda, monto, c) {
-
-
-            getFetch(otramoneda.url)
-                .then(data => data.json())
-                // .then(data => {
-                //     console.log("Datos desde el fetch ", data);
-                // })
-                .then(data => {
-                    otramoneda.obtenerTipoDeMoneda(data);
-                    console.log(otramoneda.tipoDeCambio);
-                    return getFetch(this.url);
-                })
-                .then(data => data.json())
-                .then(data =>{
-                    this.obtenerTipoDeMoneda(data);
-                    console.log(this.tipoDeCambio);                 
-                })
-                .catch(error => {
-                    alert('Error en peticiones');
-                });
-
-
-        // console.log(this.tipoDeCambio);
-
-        // otramoneda.tipoDeCambio = otramoneda.obtenerTipoDeCambio();
-        // console.log(otramoneda.tipoDeCambio);
-
-        var result = (monto / otramoneda.tipoDeCambio) * this.tipoDeCambio;
-
-        result = parseFloat(result).toFixed(6);
-        c.innerHTML = (monto + ' ' + otramoneda.nombre + ' = ' + result + this.nombre);
-
-    }
+  this.convertir = function (otramoneda, monto, c) {
+    fetch(otramoneda.url)
+      .then((data1) => data1.json())
+      .then((data1) => {
+        otramoneda.obtenerTipoDeMoneda(data1);
+        return fetch(this.url)
+          .then((data2) => data2.json())
+          .then((data2) => {
+            this.obtenerTipoDeMoneda(data2);
+            var result = (monto / otramoneda.tipoDeCambio) * this.tipoDeCambio;
+            result = parseFloat(result).toFixed(6);
+            c.innerHTML =
+              monto + " " + otramoneda.nombre + " = " + result + this.nombre;
+          });
+      })
+      .catch((error) => {
+        alert("Error en peticiones");
+      });
+  };
 }
 
 var ARS = new moneda("ARS", 86.77, urlArs);
 
-var BTC = new moneda("BTC", (1 / 8321.77), urlBtc);
+var BTC = new moneda("BTC", 1 / 8321.77, urlBtc);
 
-var BSS = new moneda("BSS", 199996.77, urlDt);
+var BSS = new moneda("BSS", 37.77, urlDt);
 
 var USD = new moneda("USD", 1, urlDt);
 
 var monedas = [ARS, BTC, BSS, USD];
 
 function convertirMoneda() {
-
-    var _de = document.getElementById("de").value;
-    var _a = document.getElementById("a").value;
-    var _monto = document.getElementById('monto').value;
-    var n1, n2;
-    var _conversion = document.getElementById("conversion");
-    var moneda1 = new moneda();
-    var moneda2 = new moneda();
-
-    if (_de == _a) {
-        _conversion.innerHTML = ('Debe colocar dos tipos diferentes de moneda en las opciones');
-    } else {
-
-        monedas.forEach(m => {
-            if (_de == m.nombre) {
-                moneda1 = m;
-            }
-            if (_a == m.nombre) {
-                moneda2 = m;
-            }
-        });
-        console.log('mapeando la moneda de ' + moneda1.nombre + ' a ' + moneda2.nombre);
-
-        moneda2.convertir(moneda1, _monto, _conversion);
-    }
+  var _de = document.getElementById("de").value;
+  var _a = document.getElementById("a").value;
+  var _monto = document.getElementById("monto").value;
+  var _conversion = document.getElementById("conversion");
+  var moneda1 = new moneda();
+  var moneda2 = new moneda();
+  if (_monto == "") {
+    _conversion.innerHTML = "Debe colocar un monto para hacer una conversion";
+  } else if (_de == _a) {
+    _conversion.innerHTML =
+      "Debe colocar dos tipos diferentes de moneda en las opciones";
+  } else {
+    //mapeando la moneda de ' + moneda1.nombre + ' a ' + moneda2.nombre
+    monedas.forEach((m) => {
+      if (_de == m.nombre) {
+        moneda1 = m;
+      }
+      if (_a == m.nombre) {
+        moneda2 = m;
+      }
+    });
+    //CONVIRTIENDO MONEDA
+    moneda2.convertir(moneda1, _monto, _conversion);
+  }
 }
 
-
 function obtenerDatosBTC() {
+  resultado.innerHTML = "";
+  spinner.show();
 
-    resultado.innerHTML=''
-    spinner.show();
-
-    getFetch(urlBtc).
-    then( data => data.json())
-    .then(datos =>{
-        console.log(datos);
-        let resultado = document.querySelector('#resultado');  
-        spinner.hide();    
-        resultado.innerHTML = `
+  fetch(urlBtc)
+    .then((data) => data.json())
+    .then((datos) => {
+      spinner.hide();
+      resultado.innerHTML = `
+        </br>
             <h2>Precio BTC- USD</h2>
             <h1>${datos.bpi.USD.rate_float} ${datos.bpi.USD.code} ${datos.bpi.USD.symbol}</h1>
             <h2>Datos tomados en: ${datos.time.updated}</h2>
             <p>${datos.disclaimer}</p>
-            `;                   
+            `;
     })
-    .catch(error => {
-        resultado.innerHTML='Opps Algo salio mal';
-    });    
-
+    .catch((error) => {
+      resultado.innerHTML = "Opps Algo salio mal";
+    });
 }
 
 function obtenerDatosDT() {
-    resultado.innerHTML = '';
-    spinner.show();
-    getFetch(urlDt).
-    then(data => data.json())
-    .then(datos =>{
-        console.log(datos);
-        let resultado = document.querySelector('#resultado');
-
-        spinner.hide();
-        resultado.innerHTML = `
-            <h2>Precio USD - BSS (Transferencia)</h2>
-            <h1>${datos.USD.transferencia} BsS </h1>
-            <h2>Precio USD - BSS (Referencia Bitcoin)</h2>
-            <h1>${datos.USD.localbitcoin_ref} BsS </h1>
-            <h2>Datos tomados en: ${datos._timestamp.fecha}</h2>
-            <p>datos tomados de Dolar Today</p>
+  resultado.innerHTML = "";
+  spinner.show();
+  fetch("https://ve.dolarapi.com/v1/dolares/oficial")
+    .then((data) => data.json())
+    .then((datos) => {
+      spinner.hide();
+      resultado.innerHTML = `
+        </br>
+            <h2>Precio USD - BS (Transferencia)</h2>
+            <h1>${datos.promedio} Bs </h1>
+            <p>datos tomados de Dolarapi.ve</p>
             `;
     })
-    .catch(error => {
-        alert('Error en peticiones');
-    });   
+    .catch((error) => {
+      alert("Error en peticiones");
+    });
 }
 
 function obtenerDatosARS() {
-    resultado.innerHTML = '';
-    spinner.show();
-    getFetch(urlArs).
-    then( data => data.json())
-    .then(datos =>{
-        console.log(datos);
-            let ARS = parseFloat(datos.libre);
-            ARS = ARS + ((ARS * 30) / 100);
-            console.log(ARS);
-            let resultado = document.querySelector('#resultado');
-            spinner.hide();
-            resultado.innerHTML = `
-                 <h2>Precio USD - ARS (Oficial)</h2>
-                 <h1>${datos.libre} ARS$ </h1>
-                 <h2>Precio USD - ARS (Blue)</h2>
-                 <h1>${datos.blue} ARS$ </h1>
-                 <h2>Precio USD - ARS (Impuesto Pais)</h2>
-                 <h1>${ARS} ARS$ </h1>
-                 
-                 <p>datos tomados de GeekLab</p>
-                `;
+  resultado.innerHTML = "";
+  spinner.show();
+
+  fetch("https://dolarapi.com/v1/dolares/blue")
+    .then((data1) => data1.json())
+    .then((datos1) => {
+      let ARSblue = parseFloat(datos1.venta);
+
+      // Now fetch the official rate
+      return fetch("https://dolarapi.com/v1/dolares/oficial")
+        .then((data2) => data2.json())
+        .then((datos2) => {
+          let ARSoficial = parseFloat(datos2.venta);
+          // Hide spinner and show the results after both fetches are complete
+          spinner.hide();
+          resultado.innerHTML = `
+                    </br>
+                        <h2>Precio USD - ARS (Oficial)</h2>
+                        <h1>${ARSoficial} ARS$</h1>
+                        <h2>Precio USD - ARS (Blue)</h2>
+                        <h1>${ARSblue} ARS$</h1>
+                        <p>datos tomados de Dolarapi.ar</p>
+                    `;
+        });
     })
-    .catch(error => {
-        alert('Error en peticiones');
-    }); 
+    .catch((error) => {
+      spinner.hide();
+      alert("Error en peticiones");
+    });
 }
